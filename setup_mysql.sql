34a8b4e4e0a7145d1590fb358d1cc051bfd33a80
medcharfi96 <1389@holbertonschool.com>
2020-10-23 20:29:01 +0200

    add

--- setup_mysql.sql
+++ setup_mysql.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the CrossMe project
 
-CREATE DATABASE IF NOT EXISTS craftiny;
-CREATE USER IF NOT EXISTS 'craft_user'@'localhost' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON craftiny.* TO 'craft_user'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'craft_user'@'localhost';
+CREATE DATABASE IF NOT EXISTS craftiny_v1;
+CREATE USER IF NOT EXISTS 'craft_user_v1'@'localhost' IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON craftiny_v1.* TO 'craft_user_v1'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'craft_user_v1'@'localhost';
 FLUSH PRIVILEGES;
